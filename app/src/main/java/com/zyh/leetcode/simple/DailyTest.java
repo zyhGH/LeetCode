50f4754bc42b25511a83f8a3b677d0db7ab84188
zyh <395060139@qq.com>
2020-11-02 11:11:14 +0800

    今日daily

--- app/src/main/java/com/zyh/leetcode/simple/DailyTest.java
+++ app/src/main/java/com/zyh/leetcode/simple/DailyTest.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.Stack;
 
 class DailyTest1 {
@@ -196,5 +197,31 @@ class DailyTest6 {
             }
         }
         return result;
+    }
+}
+
+class DailyTest7 {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set1 = new HashSet<Integer>();
+        Set<Integer> set2 = new HashSet<Integer>();
+        for (int num : nums1) {
+            set1.add(num);
+        }
+        for (int num : nums2) {
+            set2.add(num);
+        }
+
+        Set<Integer> resultSet = new HashSet<Integer>();
+        for (int num : set1) {
+            if (set2.contains(num)) {
+                resultSet.add(num);
+            }
+        }
+        int[] resultArr = new int[resultSet.size()];
+        int index = 0;
+        for (int num : resultSet) {
+            resultArr[index++] = num;
+        }
+        return resultArr;
     }
 }
