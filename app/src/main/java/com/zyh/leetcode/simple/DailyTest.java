967fc14045b32b2d06366672f64e19ac1a55f5cf
zyh <395060139@qq.com>
2020-11-04 18:19:40 +0800

    这个题不会 看答案的

--- app/src/main/java/com/zyh/leetcode/simple/DailyTest.java
+++ app/src/main/java/com/zyh/leetcode/simple/DailyTest.java
@@ -7,6 +7,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.Stack;
+import java.util.TreeMap;
 
 class DailyTest1 {
     public boolean isPalindrome(ListNode head) {        // 1->2->2->1
@@ -250,5 +251,40 @@ class DailyTest8 {
         if (i == 0 || j == A.length - 1)
             return false;
         return i == j;
+    }
+}
+
+class DailyTest9 {
+    public int[][] insert(int[][] intervals, int[] newInterval) {   //1,2,3,5,6,7,8,10,12,16   4,8
+        int left = newInterval[0];
+        int right = newInterval[1];
+        boolean placed = false;
+
+        List<int[]> ansList = new ArrayList<int[]>();
+        for (int[] interval : intervals) {
+            if (interval[0] > right) {
+                // 在插入区间的右侧且无交集
+                if (!placed) {
+                    ansList.add(new int[]{left, right});
+                    placed = true;
+                }
+                ansList.add(interval);
+            } else if (interval[1] < left) {
+                // 在插入区间的左侧且无交集
+                ansList.add(interval);
+            } else {
+                // 与插入区间有交集，计算它们的并集
+                left = Math.min(left, interval[0]);
+                right = Math.max(right, interval[1]);
+            }
+        }
+        if (!placed) {
+            ansList.add(new int[]{left, right});
+        }
+        int[][] ans = new int[ansList.size()][2];
+        for (int i = 0; i < ansList.size(); ++i) {
+            ans[i] = ansList.get(i);
+        }
+        return ans;
     }
 }
